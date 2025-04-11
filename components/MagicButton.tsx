742fad118ec82c3bce9dc703460d6d30eafe1c2b
dophuonglinhh <s3926823@rmit.edu.vn>
2025-04-11 20:40:03 +0700

    feat(button): add hover effect for magic button

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -23,19 +23,22 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="group relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-
-      {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-            bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+            bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses} 
+            transition-colors duration-300 ease-in-out group-hover:bg-slate-900 group-hover:text-purple`}
       >
         {position === "left" && icon}
         {title}
-        {position === "right" && icon}
+        {position === "right" && icon && (
+          <span className="transition-transform duration-300 ease-in-out group-hover:translate-x-1 group-hover:-translate-y-1">
+            {icon}
+          </span>
+        )}
       </span>
     </button>
   );
